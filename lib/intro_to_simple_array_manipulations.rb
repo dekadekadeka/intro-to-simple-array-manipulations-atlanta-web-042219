c1fc820a46f1bf5cee05ff8f369a31e885329854
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-02 15:32:50 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,39 @@
+def using_push(array, string)
+  array << string
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+end
+
+def shift_with_args(array, num)
+end
+
+def using_concat(array1, array2)
+end
+
+def using_insert(array, new_elem)
+end
+
+def using_uniq(array)
+end
+
+def using_flatten(array)
+end
+
+def using_delete(array, string)
+end
+
+def using_delete_at(array, int)
+end
